d2bc4946fe70994aca41b44f1dc3b6c172439343
Timofei Gomziakov <tuk36968@temple.edu>
2024-09-01 15:29:59 -0400

    Generates a random character from A-Z.

--- random.c
+++ random.c
@@ -1,7 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 #include "random.h"
 
 char randchar(){
+    // storing all of these characters here so it doesn't matter which character encoding standard the person uses
+    char allChars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
 
+    // generate random number between 0 and 26 (26 is not included)
+    int characterIndex = rand()%26; 
+    char ourCharacter = allChars[characterIndex]; 
 
-    return 'a';
+    return ourCharacter;
 }
